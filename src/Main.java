4b04b055e40f9dcbde267c470efe218a2829f4b2
Dexter Fort <dexterkun16@gmail.com>
2023-10-27 14:37:58 +0800

    UPDATED!

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    static final MovieType[] movieTypes = {
+    public static final String ANSI_RED_BG = "\u001B[41m";
+    public static final String ANSI_GREEN = "\u001B[42m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static final MovieType[] MOVIE_TYPES = {
             new MovieType("IMAX", 100),
             new MovieType("3D", 200),
             new MovieType("IMAX 3D", 250)
     };
-    static Scanner scanner = new Scanner(System.in);
-    static TransactionHandler transactions;
-    static Movie[] movies = {
+    public static Scanner scanner = new Scanner(System.in);
+    public static TransactionHandler transactions = new TransactionHandler();
+    public static Movie[] movies = {
             new Movie("Interstellar", 200, 25),
             new Movie("The Matrix", 300, 30),
             new Movie("The Godfather", 380, 80),
@@ -19,29 +23,54 @@ public class Main {
     public static void main(String[] args) {
         boolean isContinue = true;
         int movieChoice;
-        int movieTypeChoice;
+        int movieTypeChoice = 0;
+        int ticketCount = 0;
+        double change = 0;
 
         while (isContinue) {
             movieChoice = movieSelector();
-            switch (movieChoice) {
-                case 0:
-                    System.out.println("Bye!");
-                    isContinue = false;
-                    break;
-                case 1:
-                    if (movies[movieChoice - 1].getTicketsAvailable() > 0) {
-                        System.out.println("Tickets sold out! Please select another movie.");
-                        break;
-                    }
-                    System.out.println("Tickets available!");
+            if (movieChoice == 0) {
+                System.out.println("Bye!");
+                isContinue = false;
+            } else if (movieChoice > movies.length && movieChoice < movies.length + 1) {
+                System.out.println(ANSI_RED_BG + "Invalid option!" + ANSI_RESET);
+            } else {
+                if (movies[movieChoice - 1].getTicketsAvailable() <= 0) {
+                    System.out.println(ANSI_RED_BG + "Tickets sold out! Please select another movie." + ANSI_RESET);
+                    continue;
+                }
+                System.out.println(ANSI_GREEN + "Tickets available!" + ANSI_RESET);
+                while (true) {
                     System.out.println("Please select a movie type:");
                     movieTypeChoice = movieTypeSelector();
-                    transactions.setTotalAmount(movies[movieChoice - 1].getPrice() + movieTypes[movieTypeChoice - 1].getAdditionalPrice());
+                    if (movieTypeChoice > MOVIE_TYPES.length) continue;
+                    break;
+                }
+                if (movieTypeChoice == 0) continue;
 
+                System.out.println("How many tickets would you like to purchase?:");
+                while (true) {
+                    ticketCount = scanner.nextInt();
+                    if (ticketCount > movies[movieChoice - 1].getTicketsAvailable()) {
+                        System.out.println(ANSI_RED_BG + movies[movieChoice - 1].getTicketsAvailable() + " tickets are left!" + ANSI_RESET + " Please input the right number of tickets.");
+                        ticketCount = 0;
+                        continue;
+                    }
                     break;
-                default:
-                    System.out.println("Invalid option!");
-                    break;
+                }
+                movies[movieChoice - 1].setTicketsAvailable(movies[movieChoice - 1].getTicketsAvailable() - ticketCount);
+                transactions.setTotalAmount((movies[movieChoice - 1].getPrice() + MOVIE_TYPES[movieTypeChoice - 1].getAdditionalPrice()) * ticketCount);
+
+                System.out.println("Total amount: Php" + transactions.getTotalAmount());
+                System.out.println("Input amount to pay: Php");
+                while (change <= 0) {
+                    change = transactions.makePayment(scanner.nextDouble());
+                }
+
+                System.out.println("Change: Php" + change);
+                System.out.println(ANSI_GREEN + "Thank you for your purchase!" + ANSI_RESET);
+                change = 0;
+                transactions.setTotalAmount(0);
             }
         }
     }
@@ -68,8 +97,8 @@ public class Main {
     }
 
     public static void printMovieTypes() {
-        for (int i = 0; i < movieTypes.length; i++) {
-            System.out.println("[" + (i + 1) + "] " + movieTypes[i] + " (additional price: Php" + movieTypes[i].getAdditionalPrice() + ")");
+        for (int i = 0; i < MOVIE_TYPES.length; i++) {
+            System.out.println("[" + (i + 1) + "] " + MOVIE_TYPES[i].getType() + " (additional price: Php" + MOVIE_TYPES[i].getAdditionalPrice() + ")");
         }
     }
 }
--- src/TransactionHandler.java
+++ src/TransactionHandler.java
@@ -1,10 +1,21 @@
 public class TransactionHandler {
     private static double totalAmount;
 
+    double getTotalAmount() {
+        return TransactionHandler.totalAmount;
+    }
+
     void setTotalAmount(double totalAmount) {
         TransactionHandler.totalAmount = totalAmount;
     }
-    double getTotalAmount() {
-        return totalAmount;
+
+    double makePayment(double payment) {
+        if (payment < TransactionHandler.totalAmount) {
+            System.out.println("Payment unsuccessful! Please input a proper amount.");
+            return -1;
+        } else {
+            System.out.println("Payment successful!");
+            return payment - TransactionHandler.totalAmount;
+        }
     }
 }
